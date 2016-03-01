b955b806f622e8011a61592577fc89522cfb889c
Sahak <sahak79@hotmail.com>
2016-03-02 00:44:04 +0400

    minor change in drag and drop

--- src/main/resources/templates/drag/js/drag.drop.js
+++ src/main/resources/templates/drag/js/drag.drop.js
@@ -2,11 +2,21 @@ var DragModule = (function() {
 
     var elements = document.getElementsByClassName('target');
     var currentElement;
+    var deltaPX;
+    var deltaPY;
 
     function init(){
         for (var i = 0; i < elements.length; i++) {
             document.getElementsByClassName('target')[i].addEventListener("mousedown", eleMouseDown, false);
         }
+        document.querySelector('body').onkeydown = function (e) {
+            if ( !e.metaKey ) {
+                e.preventDefault();
+            }
+            if(e.keyCode == 9){
+                console.log('tab clicked');
+            }
+        };
     }
 
     function resetZIndex() {
@@ -15,8 +25,14 @@ var DragModule = (function() {
         }
     }
 
+    function setDelta(el) {
+        deltaPX = currentElement.offsetLeft - el.x;
+        deltaPY = currentElement.offsetTop - el.y;
+    }
+
     function eleMouseDown(el) {
         currentElement = el.target;
+        setDelta(el);
         resetZIndex();
         currentElement.style.zIndex = elements.length;
         document.addEventListener("mousemove", eleMouseMove, false);
@@ -25,8 +41,8 @@ var DragModule = (function() {
     function eleMouseMove(ev) {
         var pX = ev.pageX;
         var pY = ev.pageY;
-        currentElement.style.left = pX + "px";
-        currentElement.style.top = pY + "px";
+        currentElement.style.left = pX + deltaPX + "px";
+        currentElement.style.top = pY + deltaPY + "px";
         document.addEventListener("mouseup", eleMouseUp, false);
     }
 
