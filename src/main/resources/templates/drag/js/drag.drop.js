bb1402d4b2a5d9bebeb74f6e5bfe2e837afde98c
Sahak <sahak79@hotmail.com>
2016-03-01 23:42:32 +0400

    drag and drop task completed

--- src/main/resources/templates/drag/js/drag.drop.js
+++ src/main/resources/templates/drag/js/drag.drop.js
@@ -1,28 +1,43 @@
-var ele1 = document.getElementById('target1');
-var ele2 = document.getElementById('target2');
-var ele3 = document.getElementById('target3');
+var DragModule = (function() {
 
-//ele.onmousedown = eleMouseDown;
-ele1.addEventListener ("mousedown" , eleMouseDown , false);
-ele2.addEventListener ("mousedown" , eleMouseDown , false);
-ele3.addEventListener ("mousedown" , eleMouseDown , false);
+    var elements = document.getElementsByClassName('target');
+    var currentElement;
 
-function eleMouseDown () {
-    stateMouseDown = true;
-    document.addEventListener ("mousemove" , eleMouseMove , false);
-}
+    function init(){
+        for (var i = 0; i < elements.length; i++) {
+            document.getElementsByClassName('target')[i].addEventListener("mousedown", eleMouseDown, false);
+        }
+    }
 
-function eleMouseMove (ev) {
-    console.log(ev.target.id);
-    var element = document.getElementById(ev.target.id);
-    var pX = ev.pageX;
-    var pY = ev.pageY;
-    element.style.left = pX + "px";
-    element.style.top = pY + "px";
-    document.addEventListener ("mouseup" , eleMouseUp , false);
-}
+    function resetZIndex() {
+        for (var i = 0; i < elements.length; i++) {
+            document.getElementsByClassName('target')[i].style.zIndex = i;
+        }
+    }
 
-function eleMouseUp () {
-    document.removeEventListener ("mousemove" , eleMouseMove , false);
-    document.removeEventListener ("mouseup" , eleMouseUp , false);
-}
+    function eleMouseDown(el) {
+        currentElement = el.target;
+        resetZIndex();
+        currentElement.style.zIndex = elements.length;
+        document.addEventListener("mousemove", eleMouseMove, false);
+    }
+
+    function eleMouseMove(ev) {
+        var pX = ev.pageX;
+        var pY = ev.pageY;
+        currentElement.style.left = pX + "px";
+        currentElement.style.top = pY + "px";
+        document.addEventListener("mouseup", eleMouseUp, false);
+    }
+
+    function eleMouseUp() {
+        document.removeEventListener("mousemove", eleMouseMove, false);
+        document.removeEventListener("mouseup", eleMouseUp, false);
+    }
+
+    return{
+        init : init
+    }
+})();
+
+DragModule.init();
